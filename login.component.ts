41f583058b3ea600a5070312e38bdd37a9575b75
NoorhanEssam <87790620+NoorhanEssam@users.noreply.github.com>
2021-07-31 03:03:28 +0200

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300);
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+myimage:string="assets/images/index2.jpg)";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
